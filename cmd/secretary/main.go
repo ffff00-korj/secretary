97f84a74631da3c95f8c548102c32b4efc2e7490
ilya <iborovkov97@yandex.ru>
2024-03-23 20:46:13 +0300

    Bot hello world

--- cmd/secretary/main.go
+++ cmd/secretary/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/Syfaro/telegram-bot-api"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("[INFO] App started!")
+	log.Print("Initializing the application...")
+
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+		panic(err)
+	}
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Print(err)
+		panic(err)
+	}
+
+	log.Print("Application is initialized!")
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		switch update.Message.Text {
+		case "/start":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello, i`m youre secretary.")
+			bot.Send(msg)
+		case "/help":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Here is how I can help you.")
+			bot.Send(msg)
+		default:
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I received your message, but didn't understand. Try /help")
+			bot.Send(msg)
+		}
+	}
+
+	log.Print("Application is closed!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ffff00-korj/secretary
 
 go 1.21.6
+
+require (
+	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
