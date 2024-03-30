56fbc39ac365903ef015cfe0052f3d0e3617cf9b
ilya <iborovkov97@yandex.ru>
2024-03-30 20:28:31 +0300

    refactor

--- cmd/secretary/main.go
+++ cmd/secretary/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,8 +10,14 @@ import (
 )
 
 const (
-	timeout      int = 60
-	updateOffset int = 0
+	cmdStart string = "/start"
+	cmdHelp  string = "/help"
+)
+
+const (
+	timeout        int    = 60
+	updateOffset   int    = 0
+	tgTokenEnvName string = "TELEGRAM_TOKEN"
 )
 
 func app() *tgbotapi.BotAPI {
@@ -18,7 +25,7 @@ func app() *tgbotapi.BotAPI {
 		log.Print("No .env file found")
 		panic(err)
 	}
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	bot, err := tgbotapi.NewBotAPI(os.Getenv(tgTokenEnvName))
 	if err != nil {
 		log.Print(err)
 		panic(err)
@@ -44,19 +51,22 @@ func processAnUpdate(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
 			continue
 		}
 		switch update.Message.Text {
-		case "/start":
+		case cmdStart:
 			msg := tgbotapi.NewMessage(
 				update.Message.Chat.ID,
-				"Hi, i`m been employed as your personal assistant. If you want to know what i can do type /help",
+				fmt.Sprintf(
+					"Hi, i`m been employed as your personal assistant. If you want to know what i can do type %s",
+					cmdHelp,
+				),
 			)
 			bot.Send(msg)
-		case "/help":
+		case cmdHelp:
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Here's what I can do")
 			bot.Send(msg)
 		default:
 			msg := tgbotapi.NewMessage(
 				update.Message.Chat.ID,
-				"I received your message, but did not recognize it. Try /help",
+				fmt.Sprintf("I received your message, but did not recognize it. Try %s", cmdHelp),
 			)
 			bot.Send(msg)
 		}
